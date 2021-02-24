ce819277ece33f94fcb8bcc918d306b0d7b5794c
Praveen <praveenmeka06@gmail.com>
2021-02-24 16:08:14 +0530

    deploy form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="m-10 max-w-lg p-10">
+      <form className="space-y-5 p-10">
+        <div>
+          <label className="text-gray-700" for="name">
+            Full name
+          </label>
+          <input
+            className="bg-gray-100 block w-full border-0 rounded-md focus:ring-gray-500"
+            type="text"
+            name="name"
+          />
+        </div>
+        <div>
+          <label className="text-gray-700" for="email">
+            Email address
+          </label>
+          <input
+            className="bg-gray-100 block w-full border-0 rounded-md focus:ring-gray-500"
+            type="email"
+            name="email"
+          />
+        </div>
+        <div>
+          <label className="text-gray-700" for="eventdate">
+            When is your event
+          </label>
+          <input
+            className="bg-gray-100 block w-full border-0 rounded-md focus:ring-gray-200"
+            type="date"
+            name="eventdate"
+          />
+        </div>
+        <div>
+          <label className="text-gray-700" for="event">
+            What type of event is it?
+          </label>
+          <select
+            className="bg-gray-100 block w-full border-0 rounded-md focus:ring-gray-200"
+            name="event"
+          >
+            <option value="Corporate">Corporate event</option>
+            <option value="Pulic">Wedding</option>
+            <option value="private">Birthday</option>
+            <option value="private">Other</option>
+          </select>
+        </div>
+        <div>
+          <label className="text-gray-700" for="detail">
+            Additional details
+          </label>
+          <textarea
+            className="bg-gray-100 block w-full border-0 rounded-md focus:ring-gray-200"
+            rows="3"
+            name="detail"
+          ></textarea>
+        </div>
+        <div>
+          <input
+            className="bg-gray-300 rounded-sm border-0 mr-2 checked:bg-gray-700 focus:ring-1 focus:ring-gray-400"
+            type="checkbox"
+            name="offer"
+          />
+          <label className="text-gray-700" for="offer">
+            Email me news and special offers
+          </label>
+        </div>
+      </form>
     </div>
   );
 }
